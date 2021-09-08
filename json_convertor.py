98ecfd05e8a68cd706af210257eb88ddb0f2f846
Driti Tannk <driti.tannk@thegatewaycorp.com>
2021-09-08 18:07:37 +0530

    Add Data Extraction And Conversion File
    	Add tsv to json Conversion File,
    	Add json to spacy Conversion File,
    	Add Input Files.

---
+++ json_convertor.py
@@ -0,0 +1,36 @@
+import json
+import pickle
+
+
+def spacy_file_generate(input_file=None, output_file=None):
+    try:
+        training_data = []
+
+        with open(input_file, 'r') as f:
+            lines = f.readlines()
+
+        for line in lines:
+            data = json.loads(line)
+            text = data['content']
+            entities = []
+            for annotation in data['annotation']:
+                point = annotation['points'][0]
+                labels = annotation['label']
+                if not isinstance(labels, list):
+                    labels = [labels]
+
+                for label in labels:
+                    entities.append((point['start'], point['end'] + 1, label))
+
+            training_data.append((text, {"entities": entities}))
+
+        with open(output_file, 'wb') as fp:
+            pickle.dump(training_data, fp)
+
+    except Exception as e:
+        print("Unable to process " + input_file + "\n" + "error = " + str(e))
+        return None
+
+
+if __name__ == '__main__':
+    spacy_file_generate('input/training_dataset.json', 'input/training_ds.spacy')
